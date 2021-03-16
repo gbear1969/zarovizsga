04bcf1a9054ed9c11a08e2febccdabeb2ca363e1
gbear1969 <geber.andras@freemail.hu>
2021-03-16 18:21:16 +0100

    DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class DigitsCounter {
+
     public int getCountOfDigits(String s) {
         if (s==null) return 0;
         Set<Character> numbers = new HashSet<>();
